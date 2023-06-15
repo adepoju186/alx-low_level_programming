292bd6a668d1ed93f2df6dbcfc0f43280f765168
Adepoju David <dadepoju346@gmail.com>
2023-06-15 14:41:51 -0700

    Printing text

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - Entry point
+ *Description : a code to print statement without using put or print function
+ *Return: Always 1 (Success)
+ */
+
+
+int main(void)
+{
+write(2,
+"and that piece of art is useful\" - Dora Korpar 2015-10-19\n", 59);
+return (1);
