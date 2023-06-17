416a4306646ed29994420aae7008f1f01e97e7f1
Adepoju David <dadepoju346@gmail.com>
2023-06-17 12:45:56 -0700

    Listing single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Printing the single digits of base 10
+ * Description: Without suing the printf function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digits;
+
+	for (digits = 0; digits < 10; digits++)
+
+	{
+	putchar(digits);
+	}
+
+	putchar("\n");
+
+	return (0);
+}
