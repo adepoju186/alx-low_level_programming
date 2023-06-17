1e657e1ca8494f4b2b1b7143e57d48291ef618dc
Adepoju David <dadepoju346@gmail.com>
2023-06-17 13:01:07 -0700

    Listing the alphabet in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Listing thr alphabet in reverse
+ * Description: All in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letters;
+
+	for (letters = 'z'; letter >= 'a'; letters++)
+	{
+		putchar(letters);
+	}
+
+	putchar("\n");
+
+	return (0);
+}
+
