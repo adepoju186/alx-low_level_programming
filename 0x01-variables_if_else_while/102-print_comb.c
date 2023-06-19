cad877d4fa12274a6f54b7a5f49006b8bbfd94aa
Adepoju David <dadepoju346@gmail.com>
2023-06-18 17:20:36 -0700

    Possible cominations of two two digits

---
+++ 0x01-variables_if_else_while/102-print_comb.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+ /**
+  * main - Entry
+  * Description: followed by a new line
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 100; digit1++)
+	{
+		for (digit2 = 0; digit2 < 100; digit2++)
+		{
+			if (digit1 < digit2)
+			{
+				putchar((digit1 / 10) + 48);
+				putchar((digit1 % 10) + 48);
+				putchar(' ');
+				putchar((digit2 / 10) + 48);
+				putchar((digit2 % 10) + 48);
+				if (digit1 != 98 || digit2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
