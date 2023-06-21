534ffc1968c718580f474a605993cc88e2d95855
Adepoju David <dadepoju346@gmail.com>
2023-06-20 17:28:11 -0700

    Printing the englis alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Printing the alphabet in lowercase
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char al;
+
+	al = 'a';
+	while (al <= 'z')
+	{
+		_puthcar(al);
+		al++
+		_putchar('\n');
+	}
+}
