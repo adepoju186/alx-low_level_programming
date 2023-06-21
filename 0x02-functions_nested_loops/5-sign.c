95e532fdd3165bf90fb8a2505eb6fa12d644f3ca
Adepoju David <dadepoju346@gmail.com>
2023-06-21 03:33:18 -0700

    Checking an integer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+
+#include "main.h"
+
+/**
+ *print_sign - Checks if integer is positive or negative or zero
+ *@n: integer(s) to be checked
+ *Return: 1 if positive, 0 if negative, and -1 if not an integer
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('-');
+		return ('/');
+	}
+}
