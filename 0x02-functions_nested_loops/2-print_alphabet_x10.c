35a6314976b1b6c7661b666e0d9a7b2f09e7ff14
Adepoju David <dadepoju346@gmail.com>
2023-06-21 02:43:28 -0700

    Printing the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - priting the alphabet 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char al;
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		al = 'a';
+		while (al <= 'z')
+		{
+			_putchar(al);
+			al++;
+		}
+
+		_putchar('\n');
+	}
+}
