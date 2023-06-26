b7f175d395fc2afbae715b6e1b22d11075e7b76d
Adepoju David <dadepoju346@gmail.com>
2023-06-26 04:18:06 -0700

    Swapping the values of two different integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - swapping the values of two integers
+ * using two different input parameters
+ *
+ *
+ * @a: input parameter 1
+ * @b: input parameter 2
+ *
+ * Return: Nothing
+ *
+ */
+
+
+void swap_int(int *a, int *b)
+{
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
