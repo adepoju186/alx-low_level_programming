6e4cdaaa9385b4636ae66a957a5041c1605bf9bd
Adepoju David <dadepoju346@gmail.com>
2023-06-26 04:08:45 -0700

    Changing values ofparameters

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int parameter
+ * and updates its value to 98
+ * @n: int parameter
+ * Return: nothing
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
