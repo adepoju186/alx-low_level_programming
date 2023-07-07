e63905e1949734ffa47a473f8f208650325fcd10
Adepoju David <dadepoju346@gmail.com>
2023-07-07 15:35:56 -0700

    Naming a program by itslef

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array that contains string of the program
  * Return: 0 - success
  */
-int main(argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
