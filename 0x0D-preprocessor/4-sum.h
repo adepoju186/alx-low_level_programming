33f33f2ae9532fb5be03438aaa40d32b8ce20bdd
Adepoju David <dadepoju346@gmail.com>
2023-07-17 14:09:42 -0700

    A macro to sum up numbers

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,17 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM - this adds two numbers together
+ * @x: the first number to be added
+ * @y: the secind number to be added
+ *
+ * Description:
+ * this defines a macro SUM that adds two
+ * numbers together
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+
+#endif /* SUM_H */
