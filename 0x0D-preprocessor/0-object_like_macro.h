f6b8f8db37b129ed1efd5b32ab37c1800239c7aa
Adepoju David <dadepoju346@gmail.com>
2023-07-17 13:17:59 -0700

    Defining a macro SIZE

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef _OBJECT_LIKE_MACRO_H
+#define _OBJECT_LIKE_MACRO_H
+
+/**
+ * SIZE -  Macro to define size of a buffer or array to 1024
+ *
+ * Description:
+ * this macro defines any buffer or array as 1024
+ * during the preprocessing stage
+ */
+
+#define SIZE 1024
+
+#endif /*_OBJECT_LIKE_MACRO_H*/
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Preprocesso in C
