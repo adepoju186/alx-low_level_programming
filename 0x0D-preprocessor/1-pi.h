2f8d3b63e8f9946cc3e33dc0f71308ae776f7c5f
Adepoju David <dadepoju346@gmail.com>
2023-07-17 13:24:57 -0700

    Defining a macro with PI value

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef _PI_H
+#define _PI_H
+
+/**
+ * PI - Macro to define a size buffer
+ * value: 3.14159265359
+ * Description: this defines the buffer with
+ * the mathematical value of PI
+ */
+
+#define PI 3.14159265359
+
+#endif/* _PI_H*/
